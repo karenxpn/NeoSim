371d9a2ecba375a593c728a91e9700307013d0b4
karenxpn <karenmirakyan@gmail.com>
2024-04-19 23:20:09 +0400

    Initial Commit

---
+++ NeoSim/NeoSimApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeoSimApp.swift
+//  NeoSim
+//
+//  Created by Karen Mirakyan on 19.04.24.
+//
+
+import SwiftUI
+
+@main
+struct NeoSimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeoSimUITests/NeoSimUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NeoSimUITestsLaunchTests.swift
+//  NeoSimUITests
+//
+//  Created by Karen Mirakyan on 19.04.24.
+//
+
+import XCTest
+
+final class NeoSimUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
