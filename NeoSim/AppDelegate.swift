89f3417d86d2a01b07dd98eed106073d789a2d92
karenxpn <karenmirakyan@gmail.com>
2024-04-21 11:55:27 +0400

    app delegate with firebase integration

---
+++ NeoSim/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  NeoSim
+//
+//  Created by Karen Mirakyan on 21.04.24.
+//
+
+import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
