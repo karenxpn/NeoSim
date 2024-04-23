8d3a4308034539404e95f30d704cec6862498f7a
karenxpn <karenmirakyan@gmail.com>
2024-04-23 15:37:14 +0400

    firebase auth service

--- NeoSim/Services/AuthService.swift
+++ NeoSim/Services/AuthService.swift
@@ -7,9 +7,12 @@
 
 import Foundation
 import FirebaseFirestore
+import FirebaseAuth
 
 protocol AuthServiceProtocol {
     func fetchIntro() async -> Result<[IntroductionModel], Error>
+    func sendVerificationCode(phone: String) async -> Result<Void, Error>
+    func checkVerificationCode(code: String) async -> Result<String, Error>
 }
 
 class AuthService {
@@ -24,6 +27,27 @@ extension AuthService: AuthServiceProtocol {
             return try await db.collection(Paths.introduction.rawValue)
                             .getDocuments()
                             .documents.map({ try $0.data(as: IntroductionModel.self)})
+        }
+    }
+    
+    func sendVerificationCode(phone: String) async -> Result<Void, Error> {
+        return await APIHelper.shared.voidRequest {
+            let verificationID = try await PhoneAuthProvider.provider().verifyPhoneNumber(phone, uiDelegate: nil)
+            UserDefaults.standard.set( verificationID, forKey: "authVerificationID")
+        }
+    }
+    
+    func checkVerificationCode(code: String) async -> Result<String, Error> {
+        let verificationID = UserDefaults.standard.string(forKey: "authVerificationID")
+        guard let verificationID else { return .failure(Error.self as! Error) }
+        
+        do {
+            let credential =  PhoneAuthProvider.provider().credential(withVerificationID: verificationID, verificationCode: code)
+            let uid = try await Auth.auth().signIn(with: credential).user.uid
+            return .success(uid)
+            
+        } catch {
+            return .failure(error)
         }
     }
 }
